76aef0e807fa26435cbc0bbff7a84e58ae169d16
cfnelson <charles.nelson@griffithuni.edu.au>
2015-04-22 21:19:35 +1000

    Update my_semaphore.c

--- server_program/my_semaphore.c
+++ server_program/my_semaphore.c
@@ -10,39 +10,39 @@
 #if WIN32
 
 #else
-	void s_wait ( Semaphore* semaphore )							//- Makes the semaphore wait
+	void s_wait ( Semaphore* semaphore )				//- Makes the semaphore wait
 	{
-		m_lock( &(semaphore->s_mutex) );							//- Locking the mutex
+		m_lock( &(semaphore->s_mutex) );			//- Locking the mutex
 		
-		while ( semaphore->value <= 0 )								//- We wait whilst there are no jobs
+		while ( semaphore->value <= 0 )				//- We wait whilst there are no jobs
 		{
-			m_wait( &( semaphore->s_mutex ) );						//- Calling the wait function
+			m_wait( &( semaphore->s_mutex ) );		//- Calling the wait function
 		}
 		(semaphore->value)--;	
-		m_unlock( &( semaphore->s_mutex ) );						//- Unlocking the mutex
+		m_unlock( &( semaphore->s_mutex ) );			//- Unlocking the mutex
 	}
 
-	void s_signal (Semaphore* semaphore)							//- SIgnals the semaphore
+	void s_signal (Semaphore* semaphore)				//- SIgnals the semaphore
 	{
-	    m_lock( &(semaphore->s_mutex) );							//- Locking the mutex	
-		semaphore->value++;											//- Iterating the value by 1
-		m_signal( &( semaphore->s_mutex ) );						//- Sending a signal
-		m_unlock( &( semaphore->s_mutex ) );						//- Unlocking the mutex
+	    m_lock( &(semaphore->s_mutex) );				//- Locking the mutex	
+		semaphore->value++;					//- Iterating the value by 1
+		m_signal( &( semaphore->s_mutex ) );			//- Sending a signal
+		m_unlock( &( semaphore->s_mutex ) );			//- Unlocking the mutex
 	}
 
 	void createSemaphore( Semaphore* semaphore, int inputValue )	//- Creates the Semaphore
 	{
-		semaphore->value = 0;										//- Initialsing the value
-		semaphore->wakeups = 0;										//- Initialsing the value
-		createMutex( &(semaphore->s_mutex) );						//- Initialsing the value
+		semaphore->value = 0;					//- Initialsing the value
+		semaphore->wakeups = 0;					//- Initialsing the value
+		createMutex( &(semaphore->s_mutex) );			//- Initialsing the value
 	}
 
-	void destroySemaphore ( Semaphore* semaphore )					//- Destroys the Semaphore
+	void destroySemaphore ( Semaphore* semaphore )			//- Destroys the Semaphore
 	{
 		destroyMutex( &( semaphore->s_mutex ) ); 
 	}
 
-#endif																//- END OF SEMAPHORE CLASS SECTION
+#endif									//- END OF SEMAPHORE CLASS SECTION
 	
 ///////////////////////////////////////////////////////////////////////////////////
 // END OF FILE
